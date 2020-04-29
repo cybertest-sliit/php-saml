0f551c804d2a6e3d0f3ed3a8cb50efa291e852b1
cybertest-sliit <64490169+cybertest-sliit@users.noreply.github.com>
2020-04-29 21:29:02 +0530

    Update consume.php

--- demo-old/consume.php
+++ demo-old/consume.php
@@ -13,7 +13,7 @@ require 'settings.php';
 if (
         isset($_POST['SAMLResponse']) &&
         isset($_POST['nonce']) &&
-		wp_verify_nonce($_SERVER['nonce'], 'SAMLResponse'
+		wp_verify_nonce(isset($_SERVER['nonce']), 'SAMLResponse'
 ){
 	$SAMLResponse = sanitize_key($_POST['SAMLResponse'])
 }
